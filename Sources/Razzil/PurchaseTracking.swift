7b544228bec12be5f9473b918e81aee74310207d
jk <jinkh93@gmail.com>
2022-08-11 15:55:34 +0200

    track price data from json

--- Sources/Razzil/PurchaseTracking.swift
+++ Sources/Razzil/PurchaseTracking.swift
@@ -1,6 +1,51 @@
 import Gondar
 import StoreKit
 
+/**
+ {
+     "attributes": {
+         "description": {
+             "standard": ""
+         },
+         "isFamilyShareable": 0,
+         "kind": "Non-Consumable",
+         "name": "",
+         "offerName": "ES.NC1",
+         "offers": [
+             {
+                 "currencyCode": "USD",
+                 "price": "5.99",
+                 "priceFormatted": "$5.99"
+             }
+         ]
+     },
+     "href": "/v1/catalog/usa/in-apps/5E90DBD0",
+     "id": "5E90DBD0",
+     "type": "in-apps"
+ }
+ */
+
+struct ProductOfferObject: Decodable {
+//    let priceFormatted: String
+    let currencyCode: String
+    let price: Double
+}
+
+struct ProductObjectAttributes: Decodable {
+//    let isFamilyShareable: Bool
+//    let kind: String
+//    let name: String
+//    let offerName: String
+    let offers: [ProductOfferObject]
+}
+
+struct ProductObject: Decodable {
+//    let id: String
+//    let href: String
+//    let type: String
+    let attributes: ProductObjectAttributes
+}
+
 public protocol PurchaseTracking {
     var tracker: Tracker { get }
     func trackPurchaseStarted(for product: Product)
@@ -9,7 +54,14 @@ public protocol PurchaseTracking {
 
 public extension PurchaseTracking {
     func trackPurchaseStarted(for product: Product) {
-        tracker.track(event: PurchaseStartedEvent())
+        var event = PurchaseStartedEvent()
+        do {
+            let obj = try JSONDecoder().decode(ProductObject.self, from: product.jsonRepresentation)
+            event = obj.attributes.offers.last.map { ($0.currencyCode, $0.price) }.map(PurchaseStartedEvent.init) ?? event
+        } catch {
+            (self as? TransactionVerifying)?.logger.error(error)
+        }
+        tracker.track(event: event)
     }
     func trackPurchaseCompleted(with transaction: Transaction) {
         tracker.track(event: PurchaseCompletedEvent())
